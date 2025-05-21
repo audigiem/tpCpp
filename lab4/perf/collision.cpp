3f457d73fe8d8080a5d0bb784882af935dc13021
Mattéo Gautier <Matteo.Gautier@grenoble-inp.org>
2025-05-21 16:03:08 +0200

    [FINAL] last commit, nice project

--- lab4/perf/collision.cpp
+++ lab4/perf/collision.cpp
@@ -215,9 +215,7 @@ int main(int argc, char* argv[]) {
             // update the kinetic energy each freqUpdateKineticEnergy time step
             if (step % freqUpdateKineticEnergy == 0 && kineticEnergyControl) {
                 std::cout << "Velocity control by updating the kinetic energy" << std::endl;
-                for (const auto& particle : univers.getParticles()) {
-                    particle->updateVelocityWithKineticEnergyControl(targetedKineticEnergy);
-                }
+                univers.updateKineticEnergy(targetedKineticEnergy);
             }
         }
 
